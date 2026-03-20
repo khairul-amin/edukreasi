e146047bbecb5700a475e24c81193cd766214de9
khairul amin <khairul.15421020@student.ubl.ac.id>
2026-03-20 19:23:58 +0700

    Reduce Vercel

--- api/client-config.js
+++ api/client-config.js
@@ -1,5 +1,6 @@
 import { getSupabaseConfig, requireEnv } from '../lib/config.js';
 import { methodNotAllowed, sendJson } from '../lib/http.js';
+import { nowIso } from '../lib/license-store.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
@@ -7,6 +8,14 @@ export default async function handler(req, res) {
   }
 
   try {
+    if (req.query.health === '1') {
+      return sendJson(res, 200, {
+        success: true,
+        service: 'exam-edukreasi-license-online',
+        time: nowIso()
+      });
+    }
+
     const { url } = getSupabaseConfig();
     const anonKey = requireEnv('SUPABASE_ANON_KEY');
 
